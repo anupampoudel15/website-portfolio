b33349573c74a7e60ee49947fb2300dd78f6e1b5
anupampoudel15 <anupampoudel44@gmail.com>
2024-12-17 03:45:36 +0545

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,7 +71,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: [" Elegant!", " Efficient!", " Exceptional!"],
+    strings: [" Elegant!", " Efficient!", " Happen!"],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
